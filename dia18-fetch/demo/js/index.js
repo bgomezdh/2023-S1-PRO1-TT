cf09d16d2028298c492a98cdff7af8e3b0a2b7de
bgomezdh <bgomez@digitalhouse.com>
2023-05-31 16:08:53 -0300

    agregando dia de fetch

--- dia18-fetch/demo/js/index.js
+++ dia18-fetch/demo/js/index.js
@@ -1,2 +1,27 @@
-let url ='https://randomuser.me/api/'
+/* El ENDPOINT de la API */
+let url ='https://randomuser.me/api/';
+
+/* Caputando elementos del DOM */
+let fotoPerfil = document.querySelector('.fotoPerfil');
+let fotoPerfil2 = document.querySelector('.fotoPerfil2');
+let nombre = document.querySelector('.nombre');
+
+
+fetch(url)
+.then(function(response) {
+    /* tipo de dato: JSON */
+    return response.json();
+})
+.then(function(data) {
+    
+
+    /* Tipo de dato: Obj literal o Array */
+    console.log(data.results[0].name.first);
+    fotoPerfil.src = data.results[0].picture.large;
+    fotoPerfil2.src = data.results[0].picture.large;
+    nombre.innerText = `${data.results[0].name.first},  ${data.results[0].name.last}` ;
+})
+.catch(function(error) {
+    console.log(error);
+});
 
--- dia18-fetch/demo/js/provincias.js
+++ dia18-fetch/demo/js/provincias.js
@@ -1,1 +1,25 @@
-let provincias ='https://apis.datos.gob.ar/georef/api/provincias';
+let provincias = 'https://apis.datos.gob.ar/georef/api/provincias';
+let listaProvincias = document.querySelector('.listaProvincias');
+
+fetch(provincias)
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    let contenido = "";
+
+    for (let i = 0; i < data.provincias.length; i++) {
+        console.log(data.provincias[i].nombre);
+        contenido += `<li> 
+                        <a href="./provincias/${data.provincias[i].id}">
+                        ${data.provincias[i].nombre}
+                        </a>  
+                    </li>`
+    }
+
+    listaProvincias.innerHTML = contenido
+})
+.catch(function(error) {
+    console.log(error);
+});
+
